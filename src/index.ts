61f7b89e799657760aca0936d88db177b4649e62
Chris Odicho <chris.odicho@gmail.com>
2021-10-18 13:28:20 +1100

    feat: extend hello function with version to trigger a release

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { version } from './version';
+
 export function hello(name = 'John'): string {
-  return `Hello ${name} from article-archiver`;
+  return `Hello ${name} from article-archiver v${version}`;
 }
