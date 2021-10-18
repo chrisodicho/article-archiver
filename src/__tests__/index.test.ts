a2665d7e672177bb1cd65b95f45d9aef5075ee04
Chris Odicho <chris.odicho@gmail.com>
2021-10-18 14:15:24 +1100

    feat: update spec as feat to trigger release again

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,7 +1,8 @@
 import { hello } from '../index';
+import { version } from '../version';
 
 describe('My Greeter', () => {
   it('greets', () => {
-    expect(hello()).toBe('Hello John from article-archiver v1.0.1');
+    expect(hello()).toBe(`Hello John from article-archiver v${version}`);
   });
 });
