97a9f7f0770f1ec0ebdaebd1c8a932143c3aa205
Chris Odicho <chris.odicho@gmail.com>
2021-10-22 11:59:57 +1100

    chore(enhancer): disable medium meteredContent cleanup until I can confirm why it is needed

--- src/enhancer/cleanup/_medium.com.ts
+++ src/enhancer/cleanup/_medium.com.ts
@@ -3,13 +3,17 @@ export default function cleanupMedium(document: Document): Document {
     return document;
   }
 
-  const meteredContent = document.querySelector('article.meteredContent');
+  /*
+    TODO - confirm if this block is needed.
+    I have tested this block manually with meteredContent articles
+    and it doesn't seem to do anything.
+  */
+  // const meteredContent = document.querySelector('article.meteredContent');
 
-  if (meteredContent) {
-    console.log('Found meteredContent');
-    document.body.innerHTML = '';
-    document.body.appendChild(meteredContent);
-  }
+  // if (meteredContent) {
+  //   document.body.innerHTML = '';
+  //   document.body.appendChild(meteredContent);
+  // }
 
   document.body.querySelectorAll('*').forEach((node) => {
     // Remove the unrelated highlight box at the bottom of articles
